b2a9cf61064e1145e699a7ec07773c104210435e
Oxana <creativesouloxana@gmail.com>
2025-11-28 18:05:00 +0100

    update touch

--- src/assets/models/Island.jsx
+++ src/assets/models/Island.jsx
@@ -55,12 +55,10 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
   const handleKeyDown = (e) => {
     if (e.key === "ArrowLeft") {
       if (!isRotating) setIsRotating(true);
-      islandRef.current.rotation.y += 0.1 * Math.PI;
-      rotationSpeed.current = 0.0125;
+      rotationSpeed.current = 0.005;
     } else if (e.key === "ArrowRight") {
       if (!isRotating) setIsRotating(true);
-      islandRef.current.rotation.y -= 0.1 * Math.PI;
-      rotationSpeed.current = -0.0125;
+      rotationSpeed.current = -0.005;
     }
   };
 
